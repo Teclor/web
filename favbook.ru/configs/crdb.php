89b814d70996ee20646d9052d1bf80bdce3f1982
Antony <teclorian@gmail.com>
2019-11-27 21:34:25 +0400

    Remove

--- favbook.ru/configs/crdb.php
+++
@@ -1,9 +0,0 @@
-<?php
-$connection = mysqli_connect('localhost','root','','crbooks');
-mysqli_query($connection,"SET NAMES utf8"); mysqli_query($connection, "COLLATE 'utf8'");
-if($connection==false){
-    echo 'Connection error <br>';
-    echo mysqli_connect_error();
-    exit();
-}
-?>
